e34cde0f0bf85cf77bc6136bf4afb18f954fe974
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-06-03 19:59:26 +0700

    Update Custom Status fuction

--- resources/Custom-Status.js
+++ resources/Custom-Status.js
@@ -1,10 +1,15 @@
 import data from '../configs/ElainaV2_config.json'
-if (DataStore.get("Custom-Status")) {
-    const statusMessage = data["lines"].slice().join("\\n")
 
-    await fetch("/lol-chat/v1/me", {
-        method :"PUT",
-        headers:{"content-type":"application/json"},
-        body   :`{"statusMessage":"${statusMessage}"}`
-    })
+let status = data["Custom-Status"]
+if (DataStore.get("Custom-Status")) {
+    window.setInterval( async ()=> {
+        for (let i = 0; i < status.length; i++) {
+            const statusMessage = status[i]["lines"].slice().join("\\n")
+            await fetch("/lol-chat/v1/me", {
+                method :"PUT",
+                headers:{"content-type":"application/json"},
+                body   :`{"statusMessage":"${statusMessage}"}`
+            })
+        }
+    },650*status.length)
 }
