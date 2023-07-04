8fdce26b1c23f369bf384c5cae42668511cbec44
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-07-04 11:24:02 +0700

    Update

---
+++ Main/configs/ElainaV2_config.js
@@ -0,0 +1,92 @@
+const data = {
+    //______________________________________Wallpapers______________________________//
+    
+        //Support video formats: .webm, .mkv, .mp4 (not support audio), ... idk,maybe more
+        "wallpaper_list":
+        [
+            {"file": "Elaina1.webm"},
+            {"file": "Elaina2.webm"}
+        ],
+    
+    
+    
+    //_______________________________________Audio__________________________________//
+    
+        //__Support audio formats: .mp3, .flac (not support replay), .ogg , ... idk,maybe more
+        "audio_list":
+        [
+            "少女レイ-FUMIKIRI ver.flac",
+            "If there was an Endpoint.mp3"
+        ],
+    
+    
+    
+    //________________________________Plugins Settings______________________________//
+    
+        "Custom-Status":[
+            {"lines": [
+                "⡿⠉⠄⠄⠄⠄⠈⠙⠿⠟⠛⠉⠉⠉⠄⠄⠄⠈⠉⠉⠉⠛⠛⠻⢿⣿⣿⣿⣿⣿",
+                "⠁⠄⠄⠄⢀⡴⣋⣵⣮⠇⡀⠄⠄⠄⠄⠄⠄⢀⠄⠄⠄⡀⠄⠄⠄⠈⠛⠿⠋⠉",
+                "⠄⠄⠄⢠⣯⣾⣿⡿⣳⡟⣰⣿⣠⣂⡀⢀⠄⢸⡄⠄⢀⣈⢆⣱⣤⡀⢄⠄⠄⠄",
+                "⠄⠄⠄⣼⣿⣿⡟⣹⡿⣸⣿⢳⣿⣿⣿⣿⣴⣾⢻⣆⣿⣿⣯⢿⣿⣿⣷⣧⣀⣤",
+                "⠄⠄⣼⡟⣿⠏⢀⣿⣇⣿⣏⣿⣿⣿⣿⣿⣿⣿⢸⡇⣿⣿⣿⣟⣿⣿⣿⣿⣏⠋",
+                "⡆⣸⡟⣼⣯⠏⣾⣿⢸⣿⢸⣿⣿⣿⣿⣿⣿⡟⠸⠁⢹⡿⣿⣿⢻⣿⣿⣿⣿⠄",
+                "⡇⡟⣸⢟⣫⡅⣶⢆⡶⡆⣿⣿⣿⣿⣿⢿⣛⠃⠰⠆⠈⠁⠈⠙⠈⠻⣿⢹⡏⠄",
+                "⣧⣱⡷⣱⠿⠟⠛⠼⣇⠇⣿⣿⣿⣿⣿⣿⠃⣰⣿⣿⡆⠄⠄⠄⠄⠄⠉⠈⠄⠄",
+                "⡏⡟⢑⠃⡠⠂⠄⠄⠈⣾⢻⣿⣿⡿⡹⡳⠋⠉⠁⠉⠙⠄⢀⠄⠄⠄⠄⠄⠂⠄",
+                "⡇⠁⢈⢰⡇⠄⠄⡙⠂⣿⣿⣿⣿⣱⣿⡗⠄⠄⠄⢀⡀⠄⠈⢰⠄⠄⠄⠐⠄⠄",
+                "⠄⠄⠘⣿⣧⠴⣄⣡⢄⣿⣿⣿⣷⣿⣿⡇⢀⠄⠤⠈⠁⣠⣠⣸⢠⠄⠄⠄⠄⠄",
+                "⢀⠄⠄⣿⣿⣷⣬⣵⣿⣿⣿⣿⣿⣿⣿⣷⣟⢷⡶⢗⡰⣿⣿⠇⠘⠄⠄⠄⠄⠄",
+                "⣿⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣾⣿⣿⡟⢀⠃⠄⢸⡄⠁⣸",
+                "⣿⠄⠄⠘⢿⣿⣿⣿⣿⣿⣿⢛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⢄⡆⠄⢀⣪⡆⠄⣿",
+                "⡟⠄⠄⠄⠄⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣟⣻⣩⣾⣃⣴⣿⣿⡇⠸⢾"
+            ]}
+        ],
+        //add this for more object: {"lines": []},
+    
+    //___________________________________Theme Icons______________________________//
+    
+        "Animation-logo": "ElainaFly.gif",
+        "Static-logo"   : "Logo.png",
+        "Avatar"        : "Avatar.gif",
+        "RP-icon"       : "ApprenticeWitch.png",
+        "BE-icon"       : "ElainaStare.png",
+        "Rank-icon"     : "Rank.png",
+        "Emblem"        : "emblem_5.png",
+        "Class-banner"  : "Clash.png",
+        "Ticker"        : "ticker.png",
+        "Hover-card"    : "Hover-card-backdrop.jpg",
+        "Mouse-cursor"  : "ElainaDed.png",
+        
+        "Custom-Font":"",
+            "Font-Name"     : "AegirSeaborn.ttf",
+    
+    //____________________________________Filters___________________________________//
+    
+        "Homepage"    : "brightness(0.8)",
+    
+        "Clash"       : "brightness(0.2) blur(10px)",
+    
+        "Profiles"	  : "brightness(0.3)",
+    
+        "Loot"		  : "brightness(0.3)",
+    
+        "Store"       : "brightness(0.2)",
+    
+        "Yourshop"    : "brightness(0.4) blur(3px) saturate(1.5)",
+    
+        "Collections" : "brightness(0.2)",
+    
+        "Parties"     : "brightness(0.4)",
+    
+        "Champ-select": "brightness(0.4) blur(3px) saturate(1.5)",
+    
+        "Gamestart"   : "brightness(0.4) blur(10px) saturate(1.5)",
+    
+        "Postgame"    : "brightness(0.3)",
+    
+        "TFT"         : "brightness(0.8)",
+    
+    //______________________________________________________________________________//
+}
+export default data
