e079770ebd498bdba47fabd8c957fb828d221db6
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-07-04 16:14:28 +0700

    Update Buy-all-champs.js

--- Main/resources/Buy-all-champs.js
+++ Main/resources/Buy-all-champs.js
@@ -1,5 +1,5 @@
 import utils from "https://gitloaf.com/gitcfcdn/Elaina69/Elaina-V2/main/Main/_utilselaina.js"
-import axios from "https://gitcfcdn.skypack.dev/axios"
+import axios from "https://cdn.skypack.dev/axios"
 import ChampsP from 'https://gitloaf.com/gitcfcdn/Elaina69/Elaina-V2/main/Main/configs/ChampionsPrices.js'
 
 export class Store {
