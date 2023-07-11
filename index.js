10450e03cc76288fe18d28ea10686ae1a38dab4f
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-07-11 17:41:39 +0700

    remove balance-buff-viewer-plugin

--- index.js
+++ index.js
@@ -9,16 +9,16 @@
 console.log('By Elaina Da Catto');
 console.log('Meow ~~~');
 
-async function ImportPlugins(link, name) {
-	try  {let res = await fetch(link);if (res.status == 200) {(await (() => import(link))()).default}}
-	catch{console.log(`File doesn't exist, can't load ${name}`)}
-}
-
 import "./data/Data.js"
 import "./data/built-in_plugins/Custom-Status"
 import "./data/built-in_plugins/Old-LL-Settings"
 import "./data/ChangeFilters.js"
 import "./data/homepage.js"
 import "./data/loadCss.js"
-ImportPlugins('https://cdn.skypack.dev/balance-buff-viewer-plugin@latest?min', "balance-buff-viewer-plugin")
-ImportPlugins('https://fastly.jsdelivr.net/npm/elainav3-data@latest/index.js', "ElainaV3 data")
+try  {
+	let res = await fetch("https://fastly.jsdelivr.net/npm/elainav3-data@latest/index.js")
+	if (res.status == 200) {
+		(await (() => import("https://fastly.jsdelivr.net/npm/elainav3-data@latest/index.js"))()).default
+	}
+}
+catch{console.log(`File doesn't exist, can't load ElainaV3 data`)}
