d395b8740851e735c7415b856e7a0c6b001c388c
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-07-11 18:34:01 +0700

    Update Custom-Status.js

--- data/built-in_plugins/Custom-Status.js
+++ data/built-in_plugins/Custom-Status.js
@@ -1,18 +1,20 @@
 import data from '../configs/Configs.js'
 
-let time
-let i = 0
-let status = data["Custom-Status"]
-if (status.length == 1) {time = 10000}
-else {time = DataStore.get("status-delay")}
-    
-window.setInterval( async ()=> {
-    if (i == status.length - 1) {i = 0}
-    else {i++}
-    const statusMessage = status[i]["lines"].slice().join("\\n")
-        await fetch("/lol-chat/v1/me", {
-            method :"PUT",
-            headers:{"content-type":"application/json"},
-            body   :`{"statusMessage":"${statusMessage}"}`
-        }) 
-},time)
+if (DataStore.get("Custom-Status")) {
+    let time
+    let i = 0
+    let status = data["Custom-Status"]
+    if (status.length == 1) {time = 10000}
+    else {time = DataStore.get("status-delay")}
+        
+    window.setInterval( async ()=> {
+        if (i == status.length - 1) {i = 0}
+        else {i++}
+        const statusMessage = status[i]["lines"].slice().join("\\n")
+            await fetch("/lol-chat/v1/me", {
+                method :"PUT",
+                headers:{"content-type":"application/json"},
+                body   :`{"statusMessage":"${statusMessage}"}`
+            }) 
+    },time)
+}
