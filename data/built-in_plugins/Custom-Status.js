9bdc8c5a20a58ec299bdeea2033aa9911ffeda75
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-08-16 17:12:32 +0700

    new custom status

--- data/built-in_plugins/Custom-Status.js
+++ data/built-in_plugins/Custom-Status.js
@@ -1,20 +1,26 @@
-import data from '../configs/Plugins.js'
+import text from "../configs/Custom-Status.txt?raw"
 
 if (DataStore.get("Custom-Status")) {
     let time
     let i = 0
-    let status = data["Custom-Status"]
-    if (status.length == 1) {time = 10000}
+    let statusMessage = JSON.stringify(text)
+        statusMessage = statusMessage.replaceAll("\n", "\\n")
+        statusMessage = statusMessage.replaceAll("\r", "\\r")
+        statusMessage = statusMessage.replaceAll("\"", "")
+
+    let MultiStatus   = statusMessage.split("\\r\\n(end status)\\r\\n")
+
+    if   (MultiStatus.length == 1) {time = 10000}
     else {time = DataStore.get("status-delay")}
-        
+    
     window.setInterval( async ()=> {
-        if (i == status.length - 1) {i = 0}
+        if   (i == MultiStatus.length - 1) {i = 0}
         else {i++}
-        const statusMessage = status[i]["lines"].slice().join("\\n")
-            await fetch("/lol-chat/v1/me", {
-                method :"PUT",
-                headers:{"content-type":"application/json"},
-                body   :`{"statusMessage":"${statusMessage}"}`
-            }) 
+
+        await fetch("/lol-chat/v1/me", {
+            method :"PUT",
+            headers:{"content-type":"application/json"},
+            body   : `{"statusMessage": "${MultiStatus[i]}"}`
+        }) 
     },time)
 }
