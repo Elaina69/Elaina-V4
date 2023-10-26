d81fe261a2fc3000bd76cfc00e7faf63f111b5b3
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-10-26 12:44:17 +0700

    add restart key combine

--- data/built-in_plugins/KeyCombines.js
+++ data/built-in_plugins/KeyCombines.js
@@ -3,7 +3,10 @@ import {getPluginsName} from "../openFolder"
 function KeyPress(e) {
     let key = window.event? event : e
     if (key.keyCode==80 && key.ctrlKey) {
-		window.openPluginsFolder(`${getPluginsName()}`)
-	}
+        window.openPluginsFolder(`${getPluginsName()}`)
+    }
+    if (key.altKey && key.keyCode== 82) {
+        window.restartClient()
+    }
 }
-document.onkeydown = KeyPress
+document.onkeydown = KeyPress 
