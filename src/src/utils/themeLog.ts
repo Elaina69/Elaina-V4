c681f8a1b2c6a5883d755dd917f89884d326d7c1
@ Elaina @ <vipnoxhd123@gmail.com>
2025-08-31 20:28:38 +0700

    Update theme log code

--- src/src/utils/themeLog.ts
+++ src/src/utils/themeLog.ts
@@ -1,11 +1,17 @@
 const CONSOLE_STYLE = {
     prefix: '%c Elaina ',
-    css: 'color: #ffffff; background-color: #f77fbe'
+    css: 'color: #ffffff; background-color: #f77fbe',
 };
 
-export const log = (message: string, ...args: string[]) => console.log(CONSOLE_STYLE.prefix + '%c ' + message, CONSOLE_STYLE.css, '', ...args);
-export const warn = (message: string, ...args: string[]) => console.warn(CONSOLE_STYLE.prefix + '%c ' + message, CONSOLE_STYLE.css, '', ...args);
-export const error = (message: string, ...args: string[]) => console.error(CONSOLE_STYLE.prefix + '%c ' + message, CONSOLE_STYLE.css, '', ...args);
+const createLogger = (type: 'log' | 'warn' | 'error') => {
+    return (message: any, ...args: any[]) => {
+        console[type](CONSOLE_STYLE.prefix + '%c ' + message, CONSOLE_STYLE.css, '', ...args);
+    };
+};
+
+export const log = createLogger('log');
+export const warn = createLogger('warn');
+export const error = createLogger('error');
 
 // Export theme log globally
 window.log = log
